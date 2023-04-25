e5e54d534d1a73ab81eded47c9c0b8add755a2c7
Chrisanna Cornish <katkrazy123@hotmail.com>
2023-04-25 15:34:53 +0200

    added random generator

--- herocats/data/generate_random.py
+++ herocats/data/generate_random.py
@@ -1,3 +1,4 @@
+## THIS IS A TEMPLATE FROM THE MANDATORY ASSIGNMENT
 #! /usr/env/python3
 
 import random
@@ -5,10 +6,22 @@ import sys
 
 random.seed(int(sys.argv[-1])) 
 
-n = random.randrange(3, 300)
-print(n)
+def test_case():
+    M = random.randrange(1, 2000)
+    T = random.randrange(1, 500)
+    N = random.randrange(1, 50) 
 
-for _ in range(n-1):
-    print(random.randrange(-10000, 10000), end=" ")
-print(random.randrange(-10000, 10000), end="")
-print("")
+    first_line = [M, T, N]
+
+    print(*first_line)
+    
+    for _ in range(N):
+        m = random.randrange(1, M+1)
+        t = random.randrange(1, T+1)
+        r = random.randrange(1,100)
+        mission = [m, t, r]
+        print(*mission)
+
+
+if __name__ == '__main__':
+    test_case()
