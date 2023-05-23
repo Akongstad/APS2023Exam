6361f2f6c6d62a2fcbb6ac3bca309a52cd3e562d
Chrisanna Cornish <katkrazy123@hotmail.com>
2023-05-23 11:28:00 +0200

    test case that is difficult for the random solution

--- herocats/data/generate_explicit.py
+++ herocats/data/generate_explicit.py
@@ -1,7 +1,7 @@
 #! /usr/env/python3
 
 '''This test case generates biggest possible test case
-50M=1999, T=499, N=49'''
+M=1999, T=499, N=49'''
 
 import random
 random.seed(42)
--- herocats/data/generate_notrandom.py
+++ herocats/data/generate_notrandom.py
@@ -2,7 +2,7 @@
 
 '''This test case is difficult for the random solution to happen across the correct answer.'''
 
-M, T, N = 100, 100, 50
+M, T, N = 100, 100, 49
 
 cases = [[10,10,2]]*5+[[10,10,1]]*44
 print(*[M, T, N])
