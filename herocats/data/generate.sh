2b4d0d951c503efd9156cd02db21f396106efaad
Akongstad <kongstad@outlook.com>
2023-05-24 14:04:53 +0200

    add secret test cases

--- herocats/data/generate.sh
+++ herocats/data/generate.sh
@@ -11,7 +11,6 @@ compile generate_explicit.py
 sample 1
 sample 2
 sample 3
-sample 4
 
 tc  random1 generate_random 
 tc  random2 generate_random
@@ -23,3 +22,5 @@ tc  edge2 generate_explicit 250 125 100 1
 tc  edge3 generate_explicit 250 125 100 2
 tc  edge4 generate_explicit 250 125 100 4
 tc  edge5 generate_explicit 250 125 100 8
+tc  edge6 generate_explicit 250 125 100 16
+tc  edge7 generate_explicit 250 125 100 32
